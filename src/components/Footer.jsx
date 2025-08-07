1562510e4603f23342e39bb61fbdae56115557bd
isaac <lilykusi84@gmail.com>
2025-08-07 08:34:51 +0100

    Removed all files from the repository

--- src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-  return (
-    <footer>
-      <p>© {new Date().getFullYear()} Kusi Henry</p>
-      <a href="mailto:kusi14@yahoo.com">Contact Me</a>
-    </footer>
-  );
-}

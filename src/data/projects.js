1c03ea1e4392af3db10890e957f3be5eae2c35fa
isaac <lilykusi84@gmail.com>
2025-08-07 12:36:42 +0100

    Updated the work

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,71 +3,71 @@ export const projectData = [
     title: "Weekly Challenge 1 - Personal Portfolio Web Page",
     tech: "HTML, CSS",
     description: "A static landing page.",
-    image: "/assets/landing-page.png",
-    link: "https://github.com/kusiHenry/week1"
+    image: "./assets/landing-page.jpg",
+    link: "https://github.com/kusiHenry/SBC_Week_1_Challenge.git"
   },
   {
     title: "Weekly Challenge 2 - Multi-page Responsive Website",
-    tech: "JavaScript",
-    description: "A calculator app.",
-    image: "/assets/calculator.png",
-    link: "https://github.com/kusiHenry/week2"
+    tech: "HTML, CSS",
+    description: "A responsive website application",
+    image: "../assets/responsive-page.jpg",
+    link: "https://github.com/kusiHenry/responsive-website.git"
   },
   {
     title: "Weekly Challenge 3 - Migrating a Site to Bootstrap",
-    tech: "HTML, CSS",
-    description: "A static landing page.",
-    image: "/assets/landing-page.png",
-    link: "https://github.com/kusiHenry/week3"
+    tech: "HTML, CSS, Bootstrap",
+    description: "A landing page with bootstrap styling.",
+    image: "./assets/bootstrap.png",
+    link: "https://github.com/kusiHenry/bootstrap-website.git"
   },
   {
     title: "Weekly Challenge 4 - The Interactive TODO List",
-    tech: "JavaScript",
-    description: "A calculator app.",
-    image: "/assets/calculator.png",
-    link: "https://github.com/kusiHenry/week4"
+    tech: "JavaScript, HTML, CSS",
+    description: "An Interactive todo list.",
+    image: "../assets/todo.jpg",
+    link: "https://github.com/kusiHenry/todo-list.git"
   },
   {
     title: "Weekly Challenge 5 - Weather Dashboard",
     tech: "HTML, CSS",
-    description: "A static landing page.",
-    image: "/assets/landing-page.png",
-    link: "https://github.com/kusiHenry/week5"
+    description: "A weather dashboard.",
+    image: "../assets/weather-dashboard.jpg",
+    link: "https://github.com/Harle225/Bootcamp_week_4.git"
   },
   {
     title: "Weekly Challenge 6 - Python README File Generator",
     tech: "JavaScript",
-    description: "A calculator app.",
-    image: "/assets/calculator.png",
-    link: "https://github.com/kusiHenry/week6"
+    description: "A readme generator.",
+    image: "../assets/readme.jpg",
+    link: "https://github.com/kusiHenry/readme-generator.git"
   },
   {
     title: "Weekly Challenge 7 - Full Stack Notes Application using Node and Express",
     tech: "HTML, CSS",
     description: "A static landing page.",
-    image: "/assets/landing-page.png",
+    image: "../assets/landing-page.png",
     link: "https://github.com/kusiHenry/week7"
   },
   {
     title: "Weekly Challenge 8 - Full Stack Tech Blog Application",
-    tech: "JavaScript",
+    tech: "JavaScript, HTML, CSS",
     description: "A calculator app.",
-    image: "/assets/calculator.png",
-    link: "https://github.com/kusiHenry/week8"
+    image: "../assets/full-stack.jpg",
+    link: "https://github.com/kusiHenry/tech-blog-app.git"
   },
   {
     title: "Weekly Challenge 9 - React Portfolio",
-    tech: "JavaScript",
+    tech: "JavaScript, HTML, CSS",
     description: "A calculator app.",
-    image: "/assets/calculator.png",
-    link: "https://github.com/kusiHenry/week8"
+    image: "../assets/full-stack.jpg",
+    link: "https://github.com/kusiHenry/react-portfolio.git"
   },
   {
     title: "Weekly Challenge 10 - Multi-Page React Application with State Management",
     tech: "JavaScript",
     description: "A calculator app.",
-    image: "/assets/calculator.png",
-    link: "https://github.com/kusiHenry/week8"
+    image: "../assets/calculator.png",
+    link: "https://github.com/kusiHenry/week10.git"
   },
   
 ];

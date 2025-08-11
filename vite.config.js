176d98a6ddda920c021c6a0988fb2eff8b3b47fd
isaac <lilykusi84@gmail.com>
2025-08-11 17:13:26 +0100

    Added the functionality for static web rendering

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-portfolio/',
+  build: {
+    outDir: 'build',
+  },
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
 })
+
